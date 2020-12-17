5d733bf1f91251322593109793027fba136b2235
Magnus Parmo <73383707+iParmo@users.noreply.github.com>
2020-12-17 21:27:29 +0100

    You now get hints :)

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 import time
 import requests
 
-KEY = "yourkeyhere"
+KEY = "6755788fe1mshbe799a5a8347bf3p1413b5jsne93d75e37366"
 HEADERS = {"x-rapidapi-key": KEY,
 	"x-rapidapi-host": "wordsapiv1.p.rapidapi.com"}
 
 word = requests.get(url="https://wordsapiv1.p.rapidapi.com/words/?random=true", headers=HEADERS).json()['word']
+hintCount = -1
 
 guesses = int(input("How many guesses do you want? (A lot is recommended as theres alot of possible words...): "))
 seconds = 4
@@ -21,6 +22,11 @@ guess = ""
 
 while not guess == word:
     guesses -= 1
+    hintCount += 1
+    if hintCount == 0:
+        print(f'Hint: your words first letter is "{word[0]}"')
+    elif hintCount > 0:
+        print(f'Hint: {word[:hintCount]}')
     guess = input(str("Enter your guess: "))
     print(f'Guesses left: {guesses}')
     if guess == word:
